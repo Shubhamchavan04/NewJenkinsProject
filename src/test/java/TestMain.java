6e3acbd8a36c8b573193a0c47cd8fdf6371a55b1
shubham <shubhamchavan@gmail.com>
2023-02-02 03:05:40 +0530

    First Commit

---
+++ src/test/java/TestMain.java
@@ -0,0 +1,12 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class TestMain {
+ 
+ @Test
+public void testInputIsEven(){
+ assertTrue(mainClass.checkIfInputIsAnEvenNumber(23)); 
+ //Assertion
+ }
+ }
